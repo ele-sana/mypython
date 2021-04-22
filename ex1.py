2ce6d07e68a6de0dfa816cce1875f27f27aaa7c1
Eleonor Sana <eleonorsana97@gmail.com>
2021-04-22 10:58:49 +0200

    Ajout de la fonction DisplayFirstname

--- ex1.py
+++ ex1.py
@@ -4,7 +4,11 @@ _firstname=input("quel est votre prénom? ")
 def DisplayName(_name):
     print("Votre nom est: "+_name+".")
 
+def DisplayFirstname(_firstname):
+    print("Votre prénom est: "+_firstname+".")
+
 DisplayName(_name)
+DisplayFirstname(_firstname)
 
 
 
