47224748cf120956ddcfe5d26d65ad9f8d30bab4
Eleonor Sana <eleonorsana97@gmail.com>
2021-04-22 11:15:43 +0200

    Ajout de la fonction GetFullName

--- ex1.py
+++ ex1.py
@@ -7,8 +7,16 @@ def DisplayName(_name):
 def DisplayFirstname(_firstname):
     print("Votre prénom est: "+_firstname+".")
 
+def DisplayFullName(_name, _firstname):
+    if _name=="Sana" and _firstname=="Eléonor":
+        print("Bonjour princesse")
+
+    else:
+        print("Vous vous appelez "+_firstname+" "+D_name+".")
+
 DisplayName(_name)
 DisplayFirstname(_firstname)
+DisplayFullName(_name, _firstname)
 
 
 
